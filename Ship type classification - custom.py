1db58218a44444178377486ffd9fd065f35a708b
ubuntu <ubuntu@s2.ymslanda>
2018-03-15 15:15:26 +0100

    py classification for ships-not working yet

--- Ship type classification - custom.py
+++ Ship type classification - custom.py
@@ -4,9 +4,9 @@
 # In[1]:
 
 
-# get_ipython().run_line_magic('reload_ext', 'autoreload')
-# get_ipython().run_line_magic('autoreload', '2')
-# get_ipython().run_line_magic('matplotlib', 'inline')
+#get_ipython().run_line_magic('reload_ext', 'autoreload')
+#get_ipython().run_line_magic('autoreload', '2')
+#get_ipython().run_line_magic('matplotlib', 'inline')
 import platform
 python_v = platform.python_version()
 print (python_v)
@@ -53,27 +53,22 @@ def unzip(filename, path='./'):
         
 def load_dataset(path):
     data = load_files(path)
-    dog_files = np.array(data['filenames'])
-    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
-    return dog_files, dog_targets
+    ship_files = np.array(data['filenames'])
+    ship_targets = np_utils.to_categorical(np.array(data['target']), 133)
+    return ship_files, ship_targets
 
-#only the first time
-#url = 'https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip'
-#filename = download_dataset(url, path='./')
-#unzip(filename, path='./')
+train_files, train_targets = load_dataset('shipImages/train')
+valid_files, valid_targets = load_dataset('shipImages/valid')
+test_files, test_targets = load_dataset('shipImages/test')
 
-train_files, train_targets = load_dataset('dogImages/train')
-valid_files, valid_targets = load_dataset('dogImages/valid')
-test_files, test_targets = load_dataset('dogImages/test')
-
-dog_names = [item[20:-1] for item in sorted(glob("dogImages/train/*/"))]
+ship_names = [item[20:-1] for item in sorted(glob("shipImages/train/*/"))]
 
 # Let's check the dataset
-print('There are %d total dog categories.' % len(dog_names))
-print('There are %s total dog images.\n' % len(np.hstack([train_files, valid_files, test_files])))
-print('There are %d training dog images.' % len(train_files))
-print('There are %d validation dog images.' % len(valid_files))
-print('There are %d test dog images.'% len(test_files))
+print('There are %d total ship categories.' % len(ship_names))
+print('There are %s total ship images.\n' % len(np.hstack([train_files, valid_files, test_files])))
+print('There are %d training ship images.' % len(train_files))
+print('There are %d validation ship images.' % len(valid_files))
+print('There are %d test ship images.'% len(test_files))
 
 
 # ## Pre-process data
@@ -183,9 +178,9 @@ model.load_weights('saved_models/weights.best.from_scratch.hdf5')
 # In[ ]:
 
 
-# get index of predicted dog breed for each image in test set
-dog_breed_predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
+# get index of predicted ship type for each image in test set
+ship_type_predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
 # report test accuracy
-test_accuracy = 100*np.sum(np.array(dog_breed_predictions)==np.argmax(test_targets, axis=1))/len(dog_breed_predictions)
+test_accuracy = 100*np.sum(np.array(ship_type_predictions)==np.argmax(test_targets, axis=1))/len(ship_type_predictions)
 print('Test accuracy: %.4f%%' % test_accuracy)
 
